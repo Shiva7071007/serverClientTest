d439164f653050e578cccb8dab0ef36f84f67822
Shivam Kumar <shivam.kumar@intimetec.com>
2020-02-03 17:19:15 +0530

    added handling of post

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
 
 app.get('/', function(req, res){
+   console.log(req)
    res.send("hello, world!!!!");
 });
+
+app.post('/asset/pushAssetDetails', function(req, res) {
+   // console.log('POST /')
+   console.dir(req.body)
+
+
+   setTimeout(() => {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end('thanks')
+   }, 10000);
+
+
+   // res.writeHead(200, {'Content-Type': 'text/html'})
+   // res.end('thanks')
+ })
 
 app.listen(3000);
